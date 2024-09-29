d7744f7b77e2b55c8379712715ef6c72b48ab2ea
Hana Erickson <hcericks@usc.edu>
2024-09-29 14:14:28 -0700

    lab 0 updates

---
+++ lab0/intro.py
@@ -0,0 +1,31 @@
+import math 
+
+print("Hello!")
+
+x = "Another string"
+
+print (x)
+
+
+def main():
+    print("We are are inside main")
+
+    x = float(input("Please enter integer"))
+
+    if x == 10:
+        print ("x is 10!")
+    else:
+        print ("x is not 10")
+   
+if ___name____ == '___main___':
+    main()
+
+
+
+print("Your floating point is ")
+
+
+if (cond): 
+    print("This cond is true")
+
+"print "
